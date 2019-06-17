7c1e9407af06ec1a0db55482c1067645c031ad81
igor12323 <igorg12323@gmail.com>
2019-06-17 13:20:23 +0200

    Update Program.cs

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -7,8 +7,8 @@ namespace WindowsFormsApplication1
 	class Program
 	{
 		#region Dane połączenia
-		private const string Adres = "https://igordb2.documents.azure.com:443/";
-		private const string Klucz = "IEtEcVq0GiFOSOslmCiR0BeFfUF93AOYSLg3AFzs3Go0BFylY6CgU2a5wrchKf7DCpF99nxJs4jgTN11CwuCuA==";
+		private const string Adres = "";
+		private const string Klucz = "";
 		public DocumentClient client = new DocumentClient(new Uri(Adres), Klucz);
 		#endregion
 		[STAThread]
